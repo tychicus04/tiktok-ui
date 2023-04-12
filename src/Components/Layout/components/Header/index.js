7e01c5b72538c49b09c9112713a190d8999d45f0
Tychicus Nguyen <110401463+nguyenminhtai123@users.noreply.github.com>
2023-04-12 16:01:00 +0700

    Dựng khung layout mặc định

--- src/Components/Layout/components/Header/index.js
+++ src/Components/Layout/components/Header/index.js
@@ -1,5 +1,13 @@
-function Sidebar() {
-    return <h2>Sidebar</h2>;
-}
+import classNames from 'classnames/bind';
+import styles from './Header.module.scss';
 
-export default Sidebar;
+const cx = classNames.bind(styles);
+
+function Header() {
+    return (
+        <header className={cx('wrapper')}>
+            <div className={cx('inner')}></div>
+        </header>
+    );
+}
+export default Header;
