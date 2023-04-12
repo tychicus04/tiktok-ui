48e00c716c4ece070bed7f69ba74cf47196a1843
Tychicus Nguyen <110401463+nguyenminhtai123@users.noreply.github.com>
2023-04-12 15:11:56 +0700

    cấu hình router full

--- src/App.js
+++ src/App.js
@@ -1,8 +1,38 @@
+import { Fragment } from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { publicRoutes } from '~/routes/index';
+import { DefaultLayout } from './Components/Layout';
+
 function App() {
     return (
-        <div>
-            <h1>hello</h1>
-        </div>
+        <Router>
+            <div>
+                <Routes>
+                    {publicRoutes.map((route, index) => {
+                        const Page = route.component;
+                        let Layout = DefaultLayout;
+
+                        if (route.layout) {
+                            Layout = route.layout;
+                        } else if (route.layout === null) {
+                            Layout = Fragment;
+                        }
+
+                        return (
+                            <Route
+                                key={index}
+                                path={route.path}
+                                element={
+                                    <Layout>
+                                        <Page />
+                                    </Layout>
+                                }
+                            />
+                        );
+                    })}
+                </Routes>
+            </div>
+        </Router>
     );
 }
 
